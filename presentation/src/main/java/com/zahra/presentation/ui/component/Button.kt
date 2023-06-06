02f1f6f30feec5f84a8c5dc9e34e2e8179e6af1c
zahra <zahra.khodabakhshi527@gmail.com>
2023-06-07 01:33:24 +0330

    Change Layout

--- presentation/src/main/java/com/zahra/presentation/ui/component/Button.kt
+++ presentation/src/main/java/com/zahra/presentation/ui/component/Button.kt
@@ -1,17 +1,16 @@
 package com.zahra.presentation.ui.component
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.zahra.presentation.ui.shape.TextFieldBackground
 
 
 @Composable
@@ -21,15 +20,17 @@ fun Button(
 ) {
     OutlinedButton(
         onClick = { onClick() },
-        modifier = Modifier
-            .padding(8.dp)
-            .border(
-                width = 1.dp, brush = TextFieldBackground, shape = RoundedCornerShape(4.dp)
-            ),
+        modifier = Modifier.padding(8.dp),
+        colors = ButtonDefaults.outlinedButtonColors(
+            backgroundColor = DarkGray,
+            contentColor = White
+        ),
         shape = CircleShape,
     ) {
         Text(
-            text = text, fontSize = 16.sp, color = White
+            text = text,
+            fontSize = 16.sp,
+            color = White
         )
     }
 }
